2b4690d4a8c32438e7b419b8683ff4302809eb31
1efferson <jampadu@stu.ucc.edu.gh>
2025-10-22 10:10:43 +0000

    Add models/__init__.py to register models for Alembic autogenerate

--- lms/models/__init__.py
+++ lms/models/__init__.py
@@ -2,7 +2,13 @@
 # lms/models/__init__.py
 
 from lms import login_manager
+
+#  Import model modules so they are registered with SQLAlchemy metadata.
 from .user import User
+from .course import Course
+
+# export names for convenience
+__all__ = ["User", "Course"]
 
 
 # user loader 
