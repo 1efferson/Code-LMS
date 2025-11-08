8441b0427d273514c38799a5c4b5e32881dcd3f7
1efferson <jampadu@stu.ucc.edu.gh>
2025-11-08 13:56:09 +0000

    refactored instructor page

--- lms/instructor/routes.py
+++ lms/instructor/routes.py
@@ -3,6 +3,8 @@ from flask import Blueprint, render_template, abort
 from flask_login import current_user, login_required
 from lms.models import Course, Enrollment, User
 from . import instructor
+from lms.models.lesson_completion import LessonCompletion   
+from lms.main.routes import time_ago_in_words  
 
 
 @instructor.before_request
@@ -27,15 +29,37 @@ def course_students(course_id):
     if course.instructor_id != current_user.id:
         abort(403)
 
-    # Get enrollments for this course
     enrollments = course.enrollments.all()
+    students_data = []
 
-    # Get students via backref (clean!)
-    students = [enrollment.user for enrollment in enrollments]
+    for enrollment in enrollments:
+        student = enrollment.user
+
+        # --- Get Progress Stats For Each Student ---
+        courses_enrolled = student.user_enrollments.count()
+        lessons_watched = student.lesson_completions.count()
+        courses_completed = student.user_enrollments.filter_by(completed=True).count()
+
+        # Last active time
+        latest_completion = student.lesson_completions.order_by(
+            LessonCompletion.completed_at.desc()
+        ).first()
+
+        last_active = time_ago_in_words(latest_completion.completed_at) if latest_completion else "New user"
+
+        students_data.append({
+            "name": student.name,
+            "email": student.email,
+            "courses_enrolled": courses_enrolled,
+            "lessons_watched": lessons_watched,
+            "courses_completed": courses_completed,
+            "last_active": last_active
+        })
 
     return render_template(
         'instructor/course_students.html',
         course=course,
-        students=students
+        students=students_data
     )
+
 
